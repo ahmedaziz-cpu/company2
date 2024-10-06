56a3a013b564a3a10a5a4c1efcf1f65b340e6347
ahmedaziz-cpu <2024_ahmed.aziz@iticbcn.cat>
2024-10-06 14:53:52 +0200

    combinacio del fitxer amb els canvis

--- convert.js
+++ convert.js
@@ -2,7 +2,7 @@ function convertirCF() {
   let CF = prompt("A què vols convertir C/F?");
   let temp = parseFloat(prompt("Temperatura:"));
   if( CF.toUpperCase() == "C" ) {
-    let cent = (temp - 32) / (9.0 / 5.0);
+    let cent = (temp - 33) / (9.0 / 5.0);
     console.log(`${temp}F -> ${cent}C`);
   } else if (CF.toUpperCase() == "F") {
     let fh = (9.0 / 5.0) * temp + 32;
